3a9c079aa3f7862a5dbeb6f5efebe0bbe76db7ba
Matevz Cesar <mc8873@student.uni-lj.si>
2016-11-27 11:17:04 +0100

    Skoraj koncna verzija

--- landing.js
+++ landing.js
@@ -1,4 +1,4 @@
-function openCity(evt, tabname) {
+function odpriTab(evt, tabname) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("vsebinaTab");
     for (i = 0; i < tabcontent.length; i++) {
@@ -15,3 +15,30 @@ function openCity(evt, tabname) {
 function odpritab() {
     document.getElementById("prviTab").click();
 }
+//     "<table><tr><td class="votecell"><img class = "voteimg" src="icons/like.svg" alt="like"/><span>1</span><img class = "voteimg" src="icons/dislike.svg" alt="like"/></td><td class="questcell"><p> Poskusi pogooglati.</p><p>--- anon</p></td></tr><tr><td></td><td><a class="odgovorLink" onclick="komentiraj()">komentiraj</a><a class="odgovorLink">označi za neprimerno</a></td></tr></table>";
+
+function dodajOdgovor(){
+    var vsebina = document.getElementById("odgovorBoks").value;
+    document.getElementById("odgovoriContainer").innerHTML +="<table><tr><td class='votecell'><img class = 'voteimg' src='icons/like.svg' alt='like'/><span>1</span><img class = 'voteimg' src='icons/dislike.svg' alt='like'/></td><td class='questcell'><p>"+ vsebina+"</p><p>--- anon</p></td></tr><tr><td></td><td><a href='javascript:void(0)' class='odgovorLink' onclick='komentiraj(event)'>komentiraj</a>  <a class='odgovorLink'> označi za neprimerno</a></td></tr></table>";
+
+}
+
+function komentiraj(event){
+    modal.style.display = "block";
+    tabela = event.currentTarget.parentNode.parentNode.parentNode;
+}
+
+function dodajKomentar(){
+    var vsebina = document.getElementById("komentarBoks").value;
+    tabela.innerHTML += "<tr><td></td><td><table><tr><td class='votecell'><img class = 'voteimg' src='icons/like.svg' alt='like'/><span>1</span><img class = 'voteimg' src='icons/dislike.svg' alt='like'/></td><td class='questcell'><p>"+ vsebina+"</p><p>--- anon</p></td></tr><tr><td></td><td><a class='odgovorLink'> označi za neprimerno</a></td></tr></table></td></tr>";
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById("modalComment");
+var tabela= null;
