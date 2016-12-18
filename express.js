61c2d39a030afc5c815f942341d383690e165350
Matevz Cesar <mc8873@student.uni-lj.si>
2016-12-18 21:20:39 +0100

    Nacrt podatkovne baze

--- express.js
+++ express.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(__dirname + '/public')); //__dirname resolves to project folder
+
+app.use(express.static(path.join(__dirname, 'public'))); //__dirname resolves to project folder
 
 app.get('/', function (req, res) {
    res.sendFile(__dirname + '/public/landing.html');
 })
 
-app.listen(app.get('port'), function(){
+app.listen(3000, function(){
 	console.log('Express started press Ctrl-c to terimnate.');
 });
